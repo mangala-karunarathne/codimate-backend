15e21077dfc444d3f02f73ca231387d9cfc8f47d
mangala-karunarathne <mangalaicc@gmail.com>
2023-11-03 23:13:12 +0530

    CHG | change schema and routes & difference API

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express();
-const {registerUser, loginUser} =  require("../controllers/userController.js")
-
-
-router.post("/register", registerUser); // api/users/register
-router.post("/login", loginUser); // api/users/login
-
-module.exports = router;
